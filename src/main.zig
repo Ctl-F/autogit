754e964f6fc4a503678f30c53cb19b833cca1ac1
Spencer Brough <broughspencer@gmail.com>
2025-12-31 15:18:51 -0700

    branch test now working

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,25 @@
 const std = @import("std");
 const autogit = @import("autogit");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const cwd = "/home/ctlf/dev/zig/autogit/";
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    defer std.debug.assert(gpa.deinit() == .ok);
+
+    std.debug.print("Init-git\n", .{});
+    var git = try autogit.Git.init(gpa.allocator(), cwd);
+    defer git.deinit();
+
+    std.debug.print("Get Branch\n", .{});
+    const branch = try git.get_current_branch();
+    defer gpa.allocator().free(branch);
+
+    std.debug.print("Branch: {s}\n", .{branch});
+
+    var files = try git.get_file_statuses();
+    defer files.deinit(gpa.allocator());
+
+    for (files.items) |file| {
+        std.debug.print("{}\n", .{file});
+    }
+}
--- src/root.zig
+++ src/root.zig
@@ -11,18 +11,131 @@ pub const Git = struct {
     // NewBranch: git checkout -b NAME
     // Changes: git status --porcelain
 
-    pub fn get_current_branch(allocator: std.mem.Allocator, cwd: []const u8) ![]u8 {
-        const result = try std.process.Child.run(.{
+    allocator: std.mem.Allocator,
+    cwd: []const u8,
+    env: std.process.EnvMap,
+
+    pub fn init(allocator: std.mem.Allocator, cwd: []const u8) !@This() {
+        var map = try std.process.getEnvMap(allocator);
+        errdefer map.deinit();
+
+        std.debug.assert(map.get("PATH") != null);
+
+        std.debug.print("{s}\n", .{cwd});
+        const dir = try std.fs.cwd().openDir(cwd, .{});
+        std.debug.print("{}\n", .{dir});
+
+        return .{
             .allocator = allocator,
-            .argv = &.{ "git", "rev-parse", "--abbbrev-ref", "HEAD" },
             .cwd = cwd,
+            .env = map,
+        };
+    }
+
+    pub fn deinit(this: *@This()) void {
+        this.env.deinit();
+    }
+
+    pub const Status = enum {
+        Add,
+        Modify,
+        Delete,
+        Rename,
+        Conflict,
+        Untracked,
+        Unchanged,
+        Unsupported,
+    };
+
+    pub const File = struct {
+        status: Status,
+        filename: []const u8,
+        ext: []const u8,
+    };
+
+    pub fn get_current_branch(this: *@This()) ![]u8 {
+        const result = try std.process.Child.run(.{
+            .allocator = this.allocator,
+            .argv = &.{ "git", "rev-parse", "--abbrev-ref", "HEAD" },
+            .cwd = this.cwd,
+            .env_map = &this.env,
         });
 
+        return this.unwrap_result(result);
+    }
+
+    pub fn get_file_statuses(this: *@This()) !std.ArrayList(File) {
+        const result = try std.process.Child.run(.{
+            .allocator = this.allocator,
+            .argv = &.{ "git", "status", "--porcelain", "--untracked-files=all" },
+            .cwd = this.cwd,
+            .env_map = &this.env,
+        });
+
+        const stdout = this.unwrap_result(result);
+
+        defer this.allocator.free(stdout);
+        return try this.parse_files(stdout);
+    }
+
+    fn parse_files(this: *@This(), buffer: []const u8) !std.ArrayList(File) {
+        var list = try std.ArrayList(File).initCapacity(this.allocator, 128);
+        errdefer list.deinit(this.allocator);
+
+        var iter = std.mem.SplitIterator(u8, .scalar){
+            .buffer = buffer,
+            .delimiter = '\n',
+            .index = 0,
+        };
+
+        while (iter.next()) |line| {
+            if (line.len < 3) continue;
+
+            const X = line[0];
+            const Y = line[1];
+
+            const status = STAT: {
+                if (X == 'U' or Y == 'U') {
+                    break :STAT Status.Conflict;
+                }
+
+                if (X == '?' or Y == '?') {
+                    break :STAT Status.Untracked;
+                }
+
+                break :STAT switch (Y) {
+                    'M' => Status.Modify,
+                    'A' => Status.Add,
+                    'R' => Status.Rename,
+                    'D' => Status.Delete,
+                    ' ' => Status.Unchanged,
+                    else => Status.Unsupported,
+                };
+            };
+
+            const filename = buffer[3..];
+            const extPos = std.mem.lastIndexOfScalar(u8, filename, '.');
+            const ext = if (extPos) |ep|
+                buffer[ep + 1 ..]
+            else
+                "";
+
+            try list.append(this.allocator, .{
+                .status = status,
+                .filename = filename,
+                .ext = ext,
+            });
+        }
+
+        return list;
+    }
+
+    fn unwrap_result(this: *@This(), result: std.process.Child.RunResult) []u8 {
         if (result.stderr.len > 0) {
             std.debug.print("Error stream was populated: {s}\n", .{result.stderr});
         }
 
-        allocator.free(result.stderr);
+        this.allocator.free(result.stderr);
         return result.stdout;
     }
 };
